f823496617e2970897968328c22c9ffc6e69f7c1
raghavmehrotra <raghavmehrotra@rescomp-16-289098.stanford.edu>
2016-10-22 18:55:12 -0700

    First commit

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <p>Our "about" will be here</p>
+  </div>
+);
+
+export default About;
---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const App = ({ children }) => (
+  <div>
+    <header>
+      <h1>Aktashif</h1>
+      <Link to="/about">About</Link>
+    </header>
+    <section>
+      {children || 'Welcome to Aktashif'}
+    </section>
+  </div>
+);
+
+App.propTypes = { children: React.PropTypes.object };
+
+export default App;
