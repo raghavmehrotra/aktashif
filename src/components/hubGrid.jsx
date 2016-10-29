0a061baa66f14e91747350c8b2ece65d56e1bbfd
raghavmehrotra <raghavmehrotra@rescomp-16-289098.stanford.edu>
2016-10-28 22:16:06 -0700

    Styling added for caption font

--- src/components/hubGrid.jsx
+++ src/components/hubGrid.jsx
@@ -14,7 +14,7 @@ export default React.createClass({
         <Link to={"/"+name}>
           <img className="hub-homescreen-image" src={source} />
         </Link>
-        <center><p className="hub-homescreen-caption">{caption}</p></center>
+        <center><p className="hub-homescreen-image-caption">{caption}</p></center>
       </div>
     )
   },
